65269909761dc0dcac6e72ca7f6d9a3c349e54f5
Norman Lin <normanlin.net@gmail.com>
2020-03-28 19:12:56 +0800

    deploy

---
+++ precache-manifest.1c5c5513ec61936b32d86ca0f8902350.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba4e5e74700118b9b6191bb0468e3256",
+    "url": "/index.html"
+  },
+  {
+    "revision": "351de068b42eb7dcd7ce",
+    "url": "/static/js/2.2f4cb36a.chunk.js"
+  },
+  {
+    "revision": "1e11943cca4497d5abef",
+    "url": "/static/js/main.7d69d68e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5bb388b5d1c87d4b12dff9a89ab75766",
+    "url": "/static/media/exit.5bb388b5.svg"
+  },
+  {
+    "revision": "a8474d8eef7ab919939d844420c89b6e",
+    "url": "/static/media/home.a8474d8e.svg"
+  },
+  {
+    "revision": "3e5f8e367babc8286909705d3f93c10d",
+    "url": "/static/media/logo.3e5f8e36.svg"
+  },
+  {
+    "revision": "3eddd92283b12f509d52858bbf7fbf60",
+    "url": "/static/media/profile.3eddd922.svg"
+  },
+  {
+    "revision": "7dfc79232cbb090253471fe744fe5680",
+    "url": "/static/media/space.7dfc7923.jpg"
+  }
+]);
